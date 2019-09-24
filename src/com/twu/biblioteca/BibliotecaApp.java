5149d895e6fdab2a2c0ab8c727f776a207063667
astenstrasser <astenstrasser@gmail.com>
2019-09-24 14:35:35 -0300

    [Alexandra] [#1.2] View a list of all books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,32 @@
 package com.twu.biblioteca;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         welcomeMessage();
+        viewAllBooks();
     }
 
 
     public static void welcomeMessage(){
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    public static ArrayList<String> getAllBooks() {
+        ArrayList<String> allBooks = new ArrayList<>();
+        allBooks.add("Book 1");
+        allBooks.add("Book 2");
+        allBooks.add("Book 3");
+        return allBooks;
+    }
+
+    public static void viewAllBooks(){
+        ArrayList<String> books = getAllBooks();
+        books.forEach(book -> System.out.println(book));
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -21,5 +22,12 @@ public class BibliotecaAppTest {
         assertThat(outContent.toString(), is(equalTo("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n")));
     }
 
+    @Test
+    public void viewAllBooks(){
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        BibliotecaApp.viewAllBooks();
+        assertThat(outContent.toString(), is(equalTo("Book 1\nBook 2\nBook 3\n")));
+    }
 
 }
